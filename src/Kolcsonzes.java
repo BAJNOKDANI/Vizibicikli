11a734a6090a4e9310226c301340be69263d7160
BAJNOKDANI <szlobodnik.daniel.2022i@bankitatabanya.hu>
2025-05-20 17:18:29 +0200

    Kolcsonzes osztaly kesz

--- src/Kolcsonzes.java
+++ src/Kolcsonzes.java
@@ -1,4 +1,53 @@
-package PACKAGE_NAME;
-
 public class Kolcsonzes {
+    private String nev;
+    private String azonosito;
+    private String ora;
+    private String perc;
+
+    public Kolcsonzes(String nev, String azonosito, String ora, String perc) {
+        this.nev = nev;
+        this.azonosito = azonosito;
+        this.ora = ora;
+        this.perc = perc;
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+    public void setNev(String nev) {
+        this.nev = nev;
+    }
+
+    public String getAzonosito() {
+        return azonosito;
+    }
+
+    public void setAzonosito(String azonosito) {
+        this.azonosito = azonosito;
+    }
+
+    public String getOra() {
+        return ora;
+    }
+
+    public void setOra(String ora) {
+        this.ora = ora;
+    }
+
+    public String getPerc() {
+        return perc;
+    }
+
+    public void setPerc(String perc) {
+        this.perc = perc;
+    }
+
+    @Override
+    public String toString() {
+        return "nev=" + nev +
+                ", azonosito=" + azonosito +
+                ", ora=" + ora +
+                ", perc=" + perc;
+    }
 }
