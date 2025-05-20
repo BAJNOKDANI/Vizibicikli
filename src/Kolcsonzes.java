2b63a1240a16486c7f2106cb33d235fd99e68836
BAJNOKDANI <szlobodnik.daniel.2022i@bankitatabanya.hu>
2025-05-20 17:21:25 +0200

    Kolcsonzes osztaly javitas

--- src/Kolcsonzes.java
+++ src/Kolcsonzes.java
@@ -1,14 +1,18 @@
 public class Kolcsonzes {
     private String nev;
     private String azonosito;
-    private String ora;
-    private String perc;
+    private int elvora;
+    private int elvperc;
+    private int visszaOra;
+    private int visszaPerc;
 
-    public Kolcsonzes(String nev, String azonosito, String ora, String perc) {
+    public Kolcsonzes(String nev, String azonosito, int elvora, int elvperc, int visszaOra, int visszaPerc) {
         this.nev = nev;
         this.azonosito = azonosito;
-        this.ora = ora;
-        this.perc = perc;
+        this.elvora = elvora;
+        this.elvperc = elvperc;
+        this.visszaOra = visszaOra;
+        this.visszaPerc = visszaPerc;
     }
 
     public String getNev() {
@@ -27,27 +31,45 @@ public class Kolcsonzes {
         this.azonosito = azonosito;
     }
 
-    public String getOra() {
-        return ora;
+    public int getElvora() {
+        return elvora;
     }
 
-    public void setOra(String ora) {
-        this.ora = ora;
+    public void setElvora(int elvora) {
+        this.elvora = elvora;
     }
 
-    public String getPerc() {
-        return perc;
+    public int getElvperc() {
+        return elvperc;
     }
 
-    public void setPerc(String perc) {
-        this.perc = perc;
+    public void setElvperc(int elvperc) {
+        this.elvperc = elvperc;
+    }
+
+    public int getVisszaOra() {
+        return visszaOra;
+    }
+
+    public void setVisszaOra(int visszaOra) {
+        this.visszaOra = visszaOra;
+    }
+
+    public int getVisszaPerc() {
+        return visszaPerc;
+    }
+
+    public void setVisszaPerc(int visszaPerc) {
+        this.visszaPerc = visszaPerc;
     }
 
     @Override
     public String toString() {
         return "nev=" + nev +
                 ", azonosito=" + azonosito +
-                ", ora=" + ora +
-                ", perc=" + perc;
+                ", elvora=" + elvora +
+                ", elvperc=" + elvperc +
+                ", visszaOra=" + visszaOra +
+                ", visszaPerc=" + visszaPerc;
     }
 }
